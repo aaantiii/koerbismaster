518beeadd0a6847663d60f054413516e5a49fc5d
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2023-12-20 21:08:02 +0100

    Update message.go

--- message.go
+++ message.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func pingMessage(guildID, channelID, messageID string) string {
 	msg := fmt.Sprintf("<@&%s>\n", DISCORD_PING_ROLE_ID.Value())
+
+	url := fmt.Sprintf("https://discord.com/channels/%s/%s/%s\n", guildID, channelID, messageID)
 	for i := 0; i < 3; i++ {
-		msg += fmt.Sprintf("https://discord.com/channels/%s/%s/%s\n", guildID, channelID, messageID)
+		msg += url
 	}
 
 	return msg
