e153d79f260c95736694235f5c0b4cd8f48e17ab
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2023-12-23 15:10:11 +0100

    Create bot.go

---
+++ cmd/bot.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/aaantiii/koerbismaster"
+)
+
+func init() {
+	log.SetPrefix("[BOT] ")
+	log.SetFlags(log.Ldate | log.Ltime)
+
+	if err := koerbismaster.LoadEnv(); err != nil {
+		log.Fatalf("Failed to init environment variables: %v", err)
+	}
+}
+
+func main() {
+	session, err := koerbismaster.NewDiscordClient()
+	if err != nil {
+		log.Fatalf("Failed to create discord session: %v", err)
+	}
+	log.Printf("Bot is logged in as %s and running. Press CTRL-C to exit.", session.State.User.Username)
+
+	shutdownSignal := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignal, os.Interrupt)
+	<-shutdownSignal
+
+	log.Println("Gracefully shutting down...")
+	if err = session.Close(); err != nil {
+		log.Fatalf("Failed to close discord session: %v", err)
+	}
+}
