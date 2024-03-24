1a38fd82847b19738a4f1a05c63b73f921341fba
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2024-03-24 15:36:21 +0100

    remove goroutines

--- handler.go
+++ handler.go
@@ -13,44 +13,40 @@ const (
 )
 
 func handleTypingStart(s *discordgo.Session, t *discordgo.TypingStart) {
-	go func() {
-		defer handleRecovery("TypingStart")
-		if t.UserID != DISCORD_EVENT_SYS_CLIENT_ID.Value() || t.ChannelID != DISCORD_EVENT_SYS_CHANNEL_ID.Value() {
-			return
-		}
+	defer handleRecovery("TypingStart")
+	if t.UserID != DISCORD_EVENT_SYS_CLIENT_ID.Value() || t.ChannelID != DISCORD_EVENT_SYS_CHANNEL_ID.Value() {
+		return
+	}
 
-		if err := sendPingMessage(s, time.Unix(int64(t.Timestamp), 0)); err != nil {
-			log.Printf("Failed to send ping message: %v", err)
-			return
-		}
-	}()
+	if err := sendPingMessage(s, time.Unix(int64(t.Timestamp), 0)); err != nil {
+		log.Printf("Failed to send ping message: %v", err)
+		return
+	}
 }
 
 func handleMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	go func() {
-		defer handleRecovery("MessageCreate")
-		if m.Author.ID != DISCORD_EVENT_SYS_CLIENT_ID.Value() || m.ChannelID != DISCORD_EVENT_SYS_CHANNEL_ID.Value() {
+	defer handleRecovery("MessageCreate")
+	if m.Author.ID != DISCORD_EVENT_SYS_CLIENT_ID.Value() || m.ChannelID != DISCORD_EVENT_SYS_CHANNEL_ID.Value() {
+		return
+	}
+
+	if PROD {
+		if !checkMessageEmbed(m.Message) {
 			return
 		}
-
-		if PROD {
-			if !checkMessageEmbed(m.Message) {
-				return
-			}
-		} else {
-			if !checkMessageContent(m.Message) {
-				return
-			}
-		}
-
-		log.Println("MessageCreate: EventSystem sent message.")
-
-		if err := sendMessageLinks(s, m.Message); err != nil {
-			log.Printf("MessageCreate: failed to send message: %v", err)
+	} else {
+		if !checkMessageContent(m.Message) {
 			return
 		}
-		log.Printf("MessageCreate: sent message with links.")
-	}()
+	}
+
+	log.Println("MessageCreate: EventSystem sent message.")
+
+	if err := sendMessageLinks(s, m.Message); err != nil {
+		log.Printf("MessageCreate: failed to send message: %v", err)
+		return
+	}
+	log.Printf("MessageCreate: sent message with links.")
 }
 
 func handleRecovery(handlerName string) {
@@ -64,12 +60,12 @@ func checkMessageEmbed(m *discordgo.Message) bool {
 		return false
 	}
 
-	return m.Embeds[0].Title == DISCORD_EVENET_SYS_CONTENT.Value()
+	return m.Embeds[0].Title == DISCORD_EVENT_SYS_CONTENT.Value()
 }
 
 // for debugging
 func checkMessageContent(m *discordgo.Message) bool {
-	isMatch := m.Content == DISCORD_EVENET_SYS_CONTENT.Value()
+	isMatch := m.Content == DISCORD_EVENT_SYS_CONTENT.Value()
 	if !isMatch {
 		log.Println("Content does not match.")
 	}
